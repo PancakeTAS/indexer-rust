0c9f86b922efae2b17f83788f72535d55bfbd70b
PancakeTAS <pancake@mgnet.work>
2025-01-06 22:51:24 +0100

    perf: experimental performance

--- src/database/repo_indexer.rs
+++ src/database/repo_indexer.rs
@@ -1,14 +1,16 @@
 use crate::database::{handlers::on_commit_event_createorupdate, utils::unsafe_user_key_to_did};
-use anyhow::Context;
 use atrium_api::{
     record::KnownRecord,
     types::string::{Did, RecordKey},
 };
-use futures::stream::{Stream, TryStreamExt};
+use futures::stream::TryStreamExt;
 use ipld_core::cid::Cid;
 use iroh_car::CarReader;
 use log::{debug, error, info, warn};
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use tokio::sync::Semaphore;
+use tokio::runtime::Builder;
 use std::{
     collections::{BTreeMap, BTreeSet},
     sync::Arc,
@@ -17,39 +19,13 @@ use surrealdb::{engine::any::Any, Surreal};
 
 pub async fn start_full_repo_indexer(db: Surreal<Any>, max_tasks: usize) -> anyhow::Result<()> {
     let mut processed_dids: BTreeSet<String> = BTreeSet::new();
-    let (tx, rx) = async_channel::bounded(10_000);
-
     let state = Arc::new(SharedState {
-        rx: rx,
         db,
         http_client: Client::new(),
-        http_semaphore: Semaphore::new(1000),
+        http_semaphore: Semaphore::new(10000),
     });
 
-    info!(target: "indexer", "Spinning up {} handler tasks", max_tasks);
-
-    for i in 0..max_tasks {
-        let state = state.clone();
-        tokio::spawn(async move {
-            /*   thread::Builder::new()
-            .name(format!("Indexer Thread {}", i))
-            .spawn(move || {
-                Builder::new_current_thread()
-                    .enable_io()
-                    .enable_time()
-                    .build()
-                    .unwrap()
-                    .block_on(async { */
-            let res = task_handler(state, i as u64).await;
-            if let Err(e) = res {
-                error!(target: "indexer", "Handler thread {} failed: {:?}", i, e);
-            } else {
-                debug!(target: "indexer", "Handler thread {} exited", i);
-            }
-        });
-        /* })
-        .context("Failed to spawn handler thread")?; */
-    }
+    info!(target: "indexer", "Spinning up {} async handlers", max_tasks);
 
     let mut anchor = "3juj4".to_string();
     loop {
@@ -84,12 +60,21 @@ pub async fn start_full_repo_indexer(db: Surreal<Any>, max_tasks: usize) -> anyh
         }
 
         for did in dids {
-            tx.send(did)
-                .await
-                .context("Failed to send message to handler thread")?;
+            let state = state.clone();
+            tokio::spawn(async move {
+                let res = task_handler(state, did).await;
+                if let Err(e) = res {
+                    error!(target: "indexer", "Handler task failed: {:?}", e);
+                } else {
+                    debug!(target: "indexer", "Handler task exited");
+                }
+            });
+            // TODO: limit max and wait
         }
 
-        tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+        while state.http_semaphore.available_permits() < 50 {
+            tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+        }
     }
 }
 
@@ -105,49 +90,39 @@ struct BskyFollowRes {
 
 #[derive(Debug)]
 struct SharedState {
-    rx: async_channel::Receiver<String>,
     db: Surreal<Any>,
     http_client: Client,
     http_semaphore: Semaphore,
 }
 
-async fn task_handler(state: Arc<SharedState>, task_id: u64) -> anyhow::Result<()> {
-    tokio::time::sleep(std::time::Duration::from_millis(task_id * 42)).await;
-    // loop infinitely, handling repo index tasks
-    loop {
-        // get the next repo to be indexed
-        let did = {
-            let x = state.rx.recv().await;
-            x.unwrap()
-        };
-
-        let res = index_repo(&state, &did).await;
-        if let Err(e) = res {
-            let e_str = format!("{}", e);
-            if e_str == "Failed to parse CAR file: early eof" {
-                let did_key = crate::database::utils::did_to_key(did.as_str())?;
-                let timestamp_us = std::time::SystemTime::now()
-                    .duration_since(std::time::UNIX_EPOCH)
-                    .unwrap()
-                    .as_micros();
-                let _: Option<super::definitions::Record> = state
-                    .db
-                    .upsert(("li_did", did_key))
-                    .content(LastIndexedTimestamp {
-                        time_us: timestamp_us as u64,
-                        time_dt: chrono::Utc::now().into(),
-                        error: Some(e_str),
-                    })
-                    .await?;
-            } else {
-                warn!(target: "indexer", "Failed to index repo {}: {}", did, e);
-            }
-            /* match e.try_into() {
-                iroh_car::Error::Parsing(e) => {}
-                _ => {}
-            } */
+async fn task_handler(state: Arc<SharedState>, did: String) -> anyhow::Result<()> {
+    let res = index_repo(&state, &did).await;
+    if let Err(e) = res {
+        let e_str = format!("{}", e);
+        if e_str == "Failed to parse CAR file: early eof" {
+            let did_key = crate::database::utils::did_to_key(did.as_str())?;
+            let timestamp_us = std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .unwrap()
+                .as_micros();
+            let _: Option<super::definitions::Record> = state
+                .db
+                .upsert(("li_did", did_key))
+                .content(LastIndexedTimestamp {
+                    time_us: timestamp_us as u64,
+                    time_dt: chrono::Utc::now().into(),
+                    error: Some(e_str),
+                })
+                .await?;
+        } else {
+            warn!(target: "indexer", "Failed to index repo {}: {}", did, e);
         }
+        /* match e.try_into() {
+            iroh_car::Error::Parsing(e) => {}
+            _ => {}
+        } */
     }
+    Ok(())
 }
 
 async fn index_repo(state: &Arc<SharedState>, did: &String) -> anyhow::Result<()> {
